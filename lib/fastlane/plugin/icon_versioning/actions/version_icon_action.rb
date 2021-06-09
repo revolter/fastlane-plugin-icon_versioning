b7887c75f07d8503e4a9a0c17b4b816ae7d38683
Andrei Balasescu <andreibalasescu2006@gmail.com>
2021-06-09 12:40:28 +0300

    Refactor: Changed env name

--- lib/fastlane/plugin/icon_versioning/actions/version_icon_action.rb
+++ lib/fastlane/plugin/icon_versioning/actions/version_icon_action.rb
@@ -35,7 +35,7 @@ module Fastlane
           ),
           FastlaneCore::ConfigItem.new(
             key: :versioned_appiconset_suffix,
-            env_name: 'VERSIONED_ICON_VERSIONED_APPICONSET_SUFFIX',
+            env_name: 'VERSION_ICON_VERSIONED_APPICONSET_SUFFIX',
             description: 'The suffix for the versioned appiconset',
             default_value: 'Versioned',
             optional: true,
